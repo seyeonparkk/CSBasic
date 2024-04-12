8a8442e18e94cfc042dc3bebcc028d8df57a1d64
Seyeon Park <psyvitamin@gmail.com>
2024-04-12 10:10:19 +0900

    #7 int double 자료형 최대 최소 출력 지수표기법에 주의

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -21,6 +21,12 @@ namespace CSBasic
             Console.WriteLine((long)a + b);
             Console.WriteLine(a + (long)b);
             Console.WriteLine((long)a + (long)b);
+
+            //자료형 최대 최소 값 출력하기
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(double.MaxValue);
         }
     }
 }
